79f6ce676ea8e7c8dfdee92fa44e7fbb2a4981f6
Alex Biz <alex@alx.bz>
2011-11-05 17:31:15 -0400

    Change to applicationrb

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,5 @@ module Transit
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
   end
 end
