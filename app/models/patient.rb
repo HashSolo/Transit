ff2927cbfa2fdace72d187e1f7246e27d9101942
Chris Cleveland <cfclevel@gmail.com>
2012-02-24 14:49:38 -0500

    changed files

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -6,5 +6,11 @@ class Patient < ActiveRecord::Base
   has_one :history_male
   
   has_and_belongs_to_many :cases
+  
+  
+  validates :first_name, 	:presence => true
+  validates :last_name, 	:presence => true
+  validates :date_of_birth, 	:presence => true
+  validates :sex, 	:presence => true
 
 end
--- db/migrate/20120220194125_create_histories.rb
+++
@@ -1,8 +0,0 @@
-class CreateHistories < ActiveRecord::Migration
-  def change
-    create_table :histories do |t|
-      t.integer :patient_id
-      t.timestamps
-    end
-  end
-end
